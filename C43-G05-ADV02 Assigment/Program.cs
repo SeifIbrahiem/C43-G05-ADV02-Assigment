5bbcc0e831a89b569383a5ba619cae47629dfd6c
Greg Low <greg@gmail.com>
2025-01-28 07:34:36 +0200

    Example3

--- C43-G05-ADV02 Assigment/Program.cs
+++ C43-G05-ADV02 Assigment/Program.cs
@@ -2,9 +2,36 @@
 {
     internal class Program
     {
+        public static void printqueue(Queue<int> queue)
+        {
+            Console.WriteLine("Original Queue:");
+            foreach (int item in queue)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine("Reversed Queue:");
+            foreach (int item in queue)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region ex3
+            Queue<int> queue = new Queue<int>();
+            queue.Enqueue(10);
+            queue.Enqueue(20);
+            queue.Enqueue(30);
+            queue.Enqueue(40);
+            queue.Enqueue(50);
+            printqueue(queue);
+            queue.Reverse();
+            Console.WriteLine($"reverse:{queue}"); 
+            #endregion
+
+
         }
     }
 }
---
+++ C43-G05-ADV02 Assigment/Reverse.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C43_G05_ADV02_Assigment
+{
+    internal class Reverse
+    {
+        public static void ReverseQueue(Queue<int> queue)
+        {
+            Stack<int> s = new Stack<int>();
+
+            // Enqueue all elements of the queue to the stack
+            while (queue.Count > 0)
+            {
+                s.Push(queue.Dequeue());
+            }
+
+            // Dequeue all elements from the stack and enqueue them back to the queue
+            while (s.Count > 0)
+            {
+                queue.Enqueue(s.Pop());
+            }
+        }
+    }
+}
